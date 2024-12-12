158c2659882b027d7ba07e676f59f840d8ee8729
mtolkunbek@ad.salyk.kg <mtolkunbek@ad.salyk.kg>
2024-12-12 15:56:04 +0600

    AddControllers

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAuthorization();
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
